5d9d8cf3ec099628bcc6c6872bd0604f2b76a768
Suganth-S <suganthsece86@gmail.com>
2021-04-16 10:48:12 +0530

    Login Page UI Initial Commit

---
+++ android/app/src/main/kotlin/com/techzeal/login_ui_design/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.techzeal.login_ui_design
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: LoginPage(),
+  ));
+}
+
+class LoginPage extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold (
+
+      resizeToAvoidBottomInset: false,
+      body: Center(
+
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                SizedBox(
+                  child: ListView(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    physics: BouncingScrollPhysics(),
+                    children: [
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        "Welcome",
+                        style: Theme.of(context).textTheme.headline5,
+                        textAlign: TextAlign.center,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: Text(
+                          "Login",
+                          style: Theme.of(context).textTheme.subtitle2,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            labelText: "Enter Email",
+                            hintText: "abc@gmail.com",
+                            border: OutlineInputBorder()),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            labelText: "Enter Password",
+                            hintText: "******",
+                            border: OutlineInputBorder()),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50.0,
+                        child: ElevatedButton(
+                          child: Text("Log In"),
+                          onPressed: () {},
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(32.0),
+                        child: Text(
+                          "Forget Password",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                       Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "Don't Have an Account",
+                              style: Theme.of(context).textTheme.subtitle2,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Text(
+                                "Sign Up",
+                                style: TextStyle(fontSize: 17),
+                              ),
+                            ),
+                          ],
+                        ),
+                    ],
+                  ),
+                ),
+              ]),
+        ),
+      ),
+     );
+  }
+}
